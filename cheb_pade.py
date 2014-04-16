372546b0fa955b5eb0c0ca5f390cbb8ec90ba785
Henry Lambert <henry.lambert@materials.ox.ac.uk>
2014-04-16 11:43:09 +0100

    commented cheb_padepy

--- cheb_pade.py
+++ cheb_pade.py
@@ -4,87 +4,82 @@ import numpy as np
 from scipy import misc, fftpack as dft
 from matplotlib import pylab as plt
 
-def f_x(x):
-	fx = 1.0/(1.5 - np.cos(5*x)) 
-	return fx
 
+#Calculates Chebyshev points of the first kind. 
 def cheb_points(m, n, wmax):
-	N = m + n
-	points = np.array([(np.cos((2*float(j)+1)*np.pi/(2*N+2))) for j in range(N+1)], dtype=np.complex64)
-	points = points[::-1]
-	nodes = points
+  N = m + n
+  points = np.array([(np.cos((2*float(j)+1)*np.pi/(2*N+2))) for j in range(N+1)], dtype=np.complex64)
+  points = points[::-1]
+  nodes = points
 
-	for i in points:
-		print '0.0000', '\t%10.5f'%(i)
-        print 'Affine Transformation:'
-	a = -wmax
-	b = wmax
-	points = [(a+b)/2.0 + ((b-a)/2.0)*(np.cos((2*float(j)+1)*np.pi/(2*N+2))) for j in range(N+1)]
-	points = points[::-1]
-#	for i in points:
-#		print '0.0000', '\t%10.5f'%(i)
+  a = -wmax
+  b = wmax
+  points = [(a+b)/2.0 + ((b-a)/2.0)*(np.cos((2*float(j)+1)*np.pi/(2*N+2))) for j in range(N+1)]
+  points = points[::-1]
 
-	return nodes
+  return nodes
 
+#The barycentric polynomial allows one to evaluate p(x)/q(x) as
+# r(x) = \frac{\sum^{N}_{j=0} \frac{u_{j}}{x-x_{j}f_{j}}}{\sum^{N}_{j=0}\frac{u_{j}}{x-x_{j}}}
+# u_{j} = w_{j} q(x_{j}) are barycentric rational weights, and w_{j} are the barycentric polynomial weights
+# w_{j} = 1/l^{'}_{j} with l(x) = \left[\Pi_{i\neqj}^{N}(x_{i}-x_{j})\right]^{-1}, j = 0,...,N
 def bary(x, fx, nodes, qw):
-	y = np.zeros((x.shape), dtype=np.complex64)
-	fxw = np.multiply(fx[:], qw[:])
-	print qw
-	for i in range(len(x)):
-		dxinv = np.divide(1.0, (x[i] - nodes[:]))
-		#ind   = np.nonzero((np.isfinite(dxinv[:])) 
-#		ind   = np.nonzero(dxinv[:]) 
-		ind   = np.nonzero([not(a) for a in np.isfinite(dxinv[:])])
-		if len(ind) > 1:
-			print ind
-			y[i] = fx[ind]
-		else:
-			y[i] =  np.dot(fxw, dxinv)/np.dot(qw, dxinv)
-	return y
+  y = np.zeros((x.shape), dtype=np.complex64)
+  fxw = np.multiply(fx[:], qw[:])
+  print qw
+  for i in range(len(x)):
+    dxinv = np.divide(1.0, (x[i] - nodes[:]))
+    ind   = np.nonzero([not(a) for a in np.isfinite(dxinv[:])])
+    if len(ind) > 1:
+      print ind
+      y[i] = fx[ind]
+    else:
+      y[i] =  np.dot(fxw, dxinv)/np.dot(qw, dxinv)
+  return y
+
+#Code for calculation rational interpolant 
+#of the first kind using chebyshev points
 
 def ratinterp(fx, nodes, m, n):
-	N  = m + n
-	D1 = np.diag(fx)
-	#if complex might have to fft parts separately
-	D  = dft.dct(D1[:,:].real, norm='ortho')
-	D = np.transpose(D)
-	temp = D[:n,:]
-	Z  = dft.dct(temp[:n,:], norm='ortho')
-	Z  = np.transpose(Z)
-	#Z  = dft.dct(np.transpose(D[:n,:]), norm='ortho')
-	#Z  = dft.dct(D[:n,:], norm='ortho')
-	print Z
-#	Z  = dft.dct(D[:n,:], norm='ortho')
-	u, s, v = np.linalg.svd(Z[m+1:N,:])
-	#u, s, v = np.linalg.svd(Z[:,m+1:N])
-	v = v[-1,:]
-	#v = v[:,-1]
-	v = np.append(v, np.zeros((N+1)-len(v)))
-	len(v)
-	q = dft.idct(v, norm='ortho')
-	w = np.array([np.power(-1.0,j)*(np.sin((2*float(j)+1)*np.pi/(2*N+2))) for j in range(N+1)], dtype=np.complex64)
-	print v
-	print 'q', q
-	print 'w', w
-	qw = np.multiply(q,w)
-	return lambda x: bary(x, fx, nodes, qw)
+  N  = m + n
+  D1 = np.diag(fx)
+#if complex might have to fft parts separately
+  D  = dft.dct(D1[:,:].real, norm='ortho')
+  D = np.transpose(D)
+  temp = D[:n,:]
+  Z  = dft.dct(temp[:n,:], norm='ortho')
+  Z  = np.transpose(Z)
+  u, s, v = np.linalg.svd(Z[m+1:N,:])
+  v = v[-1,:]
+  v = np.append(v, np.zeros((N+1)-len(v)))
+  q = dft.idct(v, norm='ortho')
+  w = np.array([np.power(-1.0,j)*(np.sin((2*float(j)+1)*np.pi/(2*N+2))) for j in range(N+1)], dtype=np.complex64)
+  qw = np.multiply(q,w)
+  return lambda x: bary(x, fx, nodes, qw)
+
+#Trial function
+def f_x(x):
+  fx = 1.0/(1.5 - np.cos(5*x)) 
+  return fx
 
 if __name__ == '__main__':
-	m = 10
-	n = 10
-	wmax = 1
-	nodes = cheb_points(m,n,wmax)
+  m = 10
+  n = 10
+  wmax = 1
 
-	fx = np.array([f_x(x) for x in nodes], dtype=np.complex64)
-	rh = ratinterp(fx, nodes, m, n)
+  nodes = cheb_points(m,n,wmax)
 
-	xrang = np.arange(-2,2,0.01)
-	fx = np.array([f_x(x) for x in xrang])
+  fx = np.array([f_x(x) for x in nodes], dtype=np.complex64)
+  rh = ratinterp(fx, nodes, m, n)
 
-	fig = plt.figure()
-	ax1 = fig.add_subplot(111)
-	ax1.set_ylim([0,3])
-	ax1.plot(xrang,fx,'-x')
-	ax1.plot(xrang,rh(xrang),'ro')
-	plt.show()
+  xrang = np.arange(-2,2,0.01)
+  fx = np.array([f_x(x) for x in xrang])
+
+#Plot f_x (blue line) and the interpolation (red dots)
+  fig = plt.figure()
+  ax1 = fig.add_subplot(111)
+  ax1.set_ylim([0,3])
+  ax1.plot(xrang,fx,'-x')
+  ax1.plot(xrang,rh(xrang),'ro')
+  plt.show()
 	
--- pade.py
+++ pade.py
@@ -7,89 +7,95 @@ import cheb_pade as cheb
 
 #trial function
 def f_x(x):
-	fx = 1.0/(1.5 - np.cos(5*x)) 
-	return fx
+  fx = 1.0/(1.5 - np.cos(5*x)) 
+  return fx
 
 #def f_x(x):
 #	fx = 1.0/((x-2)*(x+2)) 
 #	return fx
 
 def freq(a,b,N):
-	delta = (b-a)/float(N)
-	return np.arange(a,b,delta)
+  delta = (b-a)/float(N)
+  return np.arange(a,b,delta)
 
 #vidberg-serene determine pade coefficients
 def pade_coeff(N,z,u):
-	g = np.zeros((N,N), dtype=np.complex64)
-	tmp1 = np.zeros(1, dtype=np.complex64)
-	tmp2 = np.zeros(1, dtype=np.complex64)
-	for p in range(1, N+1):
-		if p==1:
-			for i in range(1,N+1):
-				g[p-1, i-1]=u[i-1]
-		else:
-			for i in range(p,N+1):
-				tmp1   = g[p-2,p-2]/g[p-2,i-1]
-				tmp2   = g[p-2,i-1]/g[p-2,i-1]
-				g[p-1, i-1] = (tmp1 - tmp2)/(z[i-1] - z[p-2])
-	
-	a = np.zeros(N, dtype = np.complex64)
-	a = np.diagonal(g)
-	return a
+  g = np.zeros((N,N), dtype=np.complex64)
+  tmp1 = np.zeros(1, dtype=np.complex64)
+  tmp2 = np.zeros(1, dtype=np.complex64)
+  for p in range(1, N+1):
+    if p==1:
+      for i in range(1,N+1):
+        g[p-1, i-1]=u[i-1]
+    else:
+      for i in range(p,N+1):
+        tmp1   = g[p-2,p-2]/g[p-2,i-1]
+        tmp2   = g[p-2,i-1]/g[p-2,i-1]
+        g[p-1, i-1] = (tmp1 - tmp2)/(z[i-1] - z[p-2])
+
+  a = np.zeros(N, dtype = np.complex64)
+  a = np.diagonal(g)
+  return a
 
 def pade_eval(N,z,a,w):
-	acap = np.zeros(N+1, dtype=np.complex64)
-	bcap = np.zeros(N+1, dtype=np.complex64)
-	padapp = np.zeros(1, dtype=np.complex64)
+  acap = np.zeros(N+1, dtype=np.complex64)
+  bcap = np.zeros(N+1, dtype=np.complex64)
+  padapp = np.zeros(1, dtype=np.complex64)
 
-	acap[0] = complex(0.0, 0.0)
-	acap[1] = complex(a[1], 0.0)
-	bcap[0] = complex(1.0, 0.0)
-	bcap[1] = complex(1.0, 0.0)
+  acap[0] = complex(0.0, 0.0)
+  acap[1] = complex(a[0], 0.0)
+  bcap[0] = complex(1.0, 0.0)
+  bcap[1] = complex(1.0, 0.0)
 
-	for i in range(2,N+1):
-		acap[i] = acap[i-1] + (w-z[i-2]) * a[i-1] * acap[i-2]
-		bcap[i] = bcap[i-1] + (w-z[i-2]) * a[i-1] * bcap[i-2]
+  for i in range(2,N+1):
+    acap[i] = acap[i-1] + (w-z[i-2]) * a[i-1] * acap[i-2]
+    bcap[i] = bcap[i-1] + (w-z[i-2]) * a[i-1] * bcap[i-2]
 
-	padapp = acap[N]/bcap[N]
-	return padapp
+  padapp = acap[N]/bcap[N]
+  return padapp
 
 if __name__=="__main__":
-	wmax = 1
-	a1 = 0
-	b1 = 4.0
+  wmax = 1
+  a1 = -1.0
+  b1 = 1.0
 #Original function:
-	xrang = np.arange(-4,4,0.01, dtype=np.complex64)
-	xrang = xrang + complex(0.0, 0.01)
-	fx = np.array([f_x(x) for x in xrang])
+  xrang = np.arange(-4,4,0.01, dtype=np.complex64)
+	#xrang = xrang + complex(0.0, 0.01)
+  fx = np.array([f_x(x) for x in xrang])
 #create list of different order interpolants:
-	rh_list=[]
-	N = [8,12,16]
-	for n in N:
-		z = np.zeros(n, dtype=np.complex64)
-		u = np.zeros(n, dtype=np.complex64)
-		z =  np.arange(a1,b1,(b1-a1)/n)
-		z = z * complex(0,1)
+  rh_list=[]
+  sample_list=[]
+  N = [20]
+  rez=[]
+  for n in N:
+    z = np.zeros(n, dtype=np.complex64)
+    u = np.zeros(n, dtype=np.complex64)
+    z = np.arange(a1,b1,(b1-a1)/n)
+    rez.append(np.arange(a1,b1,(b1-a1)/n))
+    z = z * complex(0,1)
+    #z = z 
 #find order N interpolant
-		u = [f_x(x) for x in z]
-		a = pade_coeff(n, z, u) 
-		print z,u,a
-		rh_list.append(np.array([pade_eval(n,z,a,x) for x in xrang], dtype=np.complex64))
+    u = [f_x(x) for x in z]
+    a = pade_coeff(n, z, u) 
+    print z
+    rh_list.append(np.array([pade_eval(n,z,a,x) for x in xrang], dtype=np.complex64))
+    sample_list.append(np.array([f_x(x) for x in z], dtype=np.complex64))
 
 #Interpolated function:
-	fig = plt.figure()
-	ax1 = fig.add_subplot(111)
-	ax1.set_ylim([-2,2])
-	ax1.plot(xrang,fx, '-x')
+  fig = plt.figure()
+  ax1 = fig.add_subplot(111)
+  ax1.set_ylim([-2,2])
+  ax1.plot(xrang,fx, '-x')
 
-	fx1 = np.array([f_x(complex(0,1.0)*x) for x in xrang])
-	ax1.plot(xrang,fx1)
+  fx1 = np.array([f_x(complex(0,1.0)*x) for x in xrang])
+  ax1.plot(xrang,fx1)
 
-	for i in range(len(N)):
-		ax1.plot(xrang, np.real(rh_list[i]), label = 'N = %s'%(N[i]))
-#		ax1.plot(xrang, np.imag(rh_list[i]),'r')
-
-	plt.legend()
-	plt.show()
+  for i in range(len(N)):
+    print rez, sample_list[i]
+    ax1.plot(xrang, np.real(rh_list[i]), label = 'N = %s'%(N[i]))
+#		ax1.plot(xrang, np.imag(rh_list[i]), label = 'N = %s'%(N[i]))
+    ax1.plot(rez[i], np.real(sample_list[i]),'o', label = 'sample %s'%(N[i]))
+  plt.legend()
+  plt.show()
 
 
